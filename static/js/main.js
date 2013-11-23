be05529337ef43fb2f7ec6d35e018c04c550a36e
Smitha Milli <smitha.milli@gmail.com>
2013-11-23 15:57:41 -0800

    Add more functionality to login

--- static/js/main.js
+++ static/js/main.js
@@ -27,18 +27,38 @@ FB.Event.subscribe('auth.authResponseChange', function(response) {
     // (1) JavaScript created popup windows are blocked by most browsers unless they 
     // result from direct interaction from people using the app (such as a mouse click)
     // (2) it is a bad experience to be continually prompted to login upon page load.
-    FB.login(
-      {scope: 'read_mailbox'}
-    );
+    FB.login(function(response) {
+      if (response.authResponse) {
+        console.log('Welcome!  Fetching your information.... ');
+        FB.api('/me', function(response) {
+          console.log('Good to see you, ' + response.name + '.');
+          FB.logout(function(response) {
+            console.log('Logged out.');
+          });
+        });
+      } else {
+        console.log('User cancelled login or did not fully authorize.');
+      }
+    }, {scope: 'email'});
   } else {
     // In this case, the person is not logged into Facebook, so we call the login() 
     // function to prompt them to do so. Note that at this stage there is no indication
     // of whether they are logged into the app. If they aren't then they'll see the Login
     // dialog right after they log in to Facebook. 
     // The same caveats as above apply to the FB.login() call here.
-    FB.login(
-      {scope: 'read_mailbox'}
-    );
+    FB.login(function(response) {
+      if (response.authResponse) {
+        console.log('Welcome!  Fetching your information.... ');
+        FB.api('/me', function(response) {
+          console.log('Good to see you, ' + response.name + '.');
+          FB.logout(function(response) {
+            console.log('Logged out.');
+          });
+        });
+      } else {
+        console.log('User cancelled login or did not fully authorize.');
+      }
+    }, {scope: 'email'});
   }
 });
 };
