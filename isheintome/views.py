fe89121d1e2ce8e2d7f6afed40e9a8b7cd499680
Smitha Milli <smitha.milli@gmail.com>
2013-11-29 13:08:48 -0800

    FIX

--- isheintome/views.py
+++ isheintome/views.py
@@ -15,7 +15,6 @@ def sentiment(request):
 		access_token = request.POST['accessToken']
 		print(request.POST)
 		romInterestId = request.POST['romInterest[id]']
-		print(romInterest)
 		query = 'SELECT body FROM message WHERE thread_id IN (SELECT thread_id FROM thread WHERE folder_id=0) AND author_id=' + romInterestId
 		data = {'q' : query, 'access_token' : access_token}
 		data = urllib.urlencode(data)
